b68256bcfae2e5978cef2d69c832e6e24ed81391
Utkarsh <utkarshsinghal369@gmail.com>
2023-12-20 16:45:04 +0530

    Video Call

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,14 +7,44 @@ const Home = () => {
     navigate(`/room/${value}`);
   };
   return (
-    <div>
-      <input
-        type="text"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-        placeholder="enter your room code"
-      />
-      <button onClick={handleJoinRoom}>join</button>{" "}
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh", // Optional: Adjust the height as needed
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column", // Optional: If you want to stack input and button vertically
+          alignItems: "center",
+
+          padding: "20px", // Optional: Add padding for better appearance
+        }}
+      >
+        <input
+          style={{
+            padding: "15px 40px",
+            borderRadius: "20px",
+            border:"none"
+          }}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          placeholder="enter your room code"
+        />
+        <button style={{
+          padding:"8px 20px",
+          fontSize:"24px",
+          color:"#fff",
+          fontWeight:"700",
+          border:"none",
+          borderRadius:"30px",
+          backgroundColor:"green"
+        }} onClick={handleJoinRoom}>join</button>{" "}
+      </div>
     </div>
   );
 };
